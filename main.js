cbc1622b7e2c98fdcc670ccff2556172670cd803
Charles Totoro <crtotoro@gmail.com>
2023-11-17 14:54:03 -0600

    Update get random joke to return a single part joke

--- main.js
+++ main.js
@@ -4,12 +4,12 @@
 async function getRandomJoke() {
   let jokeResponse, jokeContent; 
   try { 
-    jokeResponse = await fetch('https://v2.jokeapi.dev/joke/Any?safe-mode&lang=en');
+    jokeResponse = await fetch('https://v2.jokeapi.dev/joke/Any?type=single&safe-mode&lang=en');
     jokeContent = await jokeResponse.json();
   }
   catch(error) { console.log(error); }
 
-  console.log(jokeContent);
+  return jokeContent.joke;
 }
 
 getRandomJoke();

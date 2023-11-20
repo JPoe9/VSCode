28a93bec010701ef4f0a50883ed5034595a33d3b
Shalini <shalini.potham.vabqu9@statefarm.com>
2023-11-20 10:28:15 -0600

    completed the getRandomExcuse function and removed the placeholder joke and excuse from home html

--- main.js
+++ main.js
@@ -12,4 +12,29 @@ async function getRandomJoke() {
   return jokeContent.joke;
 }
 
-getRandomJoke();
+// console.log(getRandomJoke());
+
+
+async function getRandomExcuse(){
+  let  excuseResponse,excuseJson;
+  try{
+    excuseResponse = await fetch("https://excuser-three.vercel.app/v1/excuse");
+    excuseJson = await excuseResponse.json();
+  }catch(error){console.log(error); 
+                return "My alarm did not go off in time to come up with an excuse!";}
+
+  return excuseJson[0].excuse;
+}
+// console.log(getRandomExcuse());
+
+ function displayRandoms(){
+  const randomJokeObj = document.querySelector("#random-joke");
+  const randomExcuseObj = document.querySelector("#random-excuse");
+  Promise.all([getRandomJoke(),getRandomExcuse()])
+  .then((results)=>{
+    randomJokeObj.innerText =  results[0];
+    randomExcuseObj.innerText =  results[1];
+  })
+}
+
+displayRandoms();

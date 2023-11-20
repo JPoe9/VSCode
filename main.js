3f583ebf2bbbe93660080103f8ec582c726dc9b2
Jenny Poe <jenny.poe.pbwr@statefarm.com>
2023-11-20 14:14:04 -0600

    adds more styling

--- main.js
+++ main.js
@@ -70,3 +70,11 @@ function displayDailyRandoms(){
 }
 
 displayDailyRandoms();
+
+let submit_button = document.getElementById("submitbutton");
+submit_button.addEventListener('click', function(event){
+const newPTag = document.createElement('p');
+newPTag.innerText = "Thank you for the suggestion";
+newPTag.classList.add("tracker");
+document.body.append(newPTag);
+})

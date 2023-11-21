f7a3f2393d0783023924fe1798263c48aa93fda4
Shalini <shalini.potham.vabqu9@statefarm.com>
2023-11-21 14:24:40 -0600

    Updated the moreJokes.js to handle the scenario where the combination of options did not yield a joke but the API call is not throwing an error for it.

--- moreJokes.js
+++ moreJokes.js
@@ -3,11 +3,13 @@ jSelectionBtn.addEventListener("click", getSelectJoke);
 
 function getSelectJoke(){
     const jContainer = document.querySelector("#jokeReturnedContainer");
+    const hElement = document.querySelector("#jokeReturnedContainer h2");
     const jPTag = document.querySelector("#jokeReturned");
     const jDeliveryPTag = document.querySelector("#jokeDelivery");
     const jDeliveryBtn = document.querySelector("#jDeliveryBtn");
     jDeliveryPTag.classList.add("hidden");
     jDeliveryBtn.classList.add("hidden");
+    hElement.innerText = " Here's the joke you requested... enjoy! ";
     jPTag.innerText = "";
     let jCategory = document.querySelector("#jokeCategory").value;
     let jType = document.querySelector("#jokeType").value
@@ -20,28 +22,35 @@ function getSelectJoke(){
         case "Any Type":
             jUrl = `https://v2.jokeapi.dev/joke/${jCategory}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`;
             break;
-        case "Single":
-            jUrl = `https://v2.jokeapi.dev/joke/${jCategory}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`;
-            break;
-        case "Two-part":
-            jUrl = `https://v2.jokeapi.dev/joke/${jCategory}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart`;
-    }
-    fetch(jUrl)
-    .then((response)=>response.json())
-    .then(responseObject => {
-        console.log(responseObject);
-        jContainer.classList.remove("hidden");
-        if(responseObject.type === "single" ){ //&& responseObject.joke
-            jPTag.innerText = responseObject.joke;
-        }else{ // responseObject.setup && responseObject.delivery
-            jPTag.innerText = responseObject.setup;
-            jDeliveryPTag.innerText = responseObject.delivery;
-            jDeliveryBtn.classList.remove("hidden");
-            jDeliveryBtn.addEventListener("click", function (){
-                jDeliveryPTag.classList.remove("hidden");
-                jDeliveryBtn.classList.add("hidden");
-            })
-        }
-    })
-    .catch(error=>console.log(error));
+            case "Single":
+                jUrl = `https://v2.jokeapi.dev/joke/${jCategory}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`;
+                break;
+                case "Two-part":
+                    jUrl = `https://v2.jokeapi.dev/joke/${jCategory}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart`;
+                    break;
+                }
+                fetch(jUrl)
+                .then((response)=>response.json())
+                .then(responseObject => {
+                    console.log(responseObject.error);
+                    if(responseObject.error){
+                        jPTag.classList.add("hidden");
+                         hElement.innerText = "The options you selected did not yield a joke. Please change the selection and try again!";
+                    }else{
+                        jPTag.classList.remove("hidden");
+                        jContainer.classList.remove("hidden");
+                        if(responseObject.type === "single" ){ //&& responseObject.joke
+                            jPTag.innerText = responseObject.joke;
+                        }else{ // responseObject.setup && responseObject.delivery
+                            jPTag.innerText = responseObject.setup;
+                            jDeliveryPTag.innerText = responseObject.delivery;
+                            jDeliveryBtn.classList.remove("hidden");
+                            jDeliveryBtn.addEventListener("click", function (){
+                                jDeliveryPTag.classList.remove("hidden");
+                                jDeliveryBtn.classList.add("hidden");
+                            })
+                        }
+                    }
+                })
+            .catch(error=>console.log(error));
 }

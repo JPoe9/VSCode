6bbb102bedf3418cde21c43845d858d76ef07db5
Rachel Coplan <rachel.coplan.mm9c@statefarm.com>
2023-11-21 10:38:58 -0700

    working on styling and stuff

--- moreExcuses.js
+++ moreExcuses.js
@@ -19,7 +19,6 @@ submitExcusesButton.addEventListener("click", function () {
   // alert("Selected value saved: " + selectedValue);
 
   let selectedExcuses;
-  let addAPTagFlag = false;
   let selectedFetch = fetch(
     `https://excuser-three.vercel.app/v1/excuse/${selectedValue}`
   )
@@ -30,14 +29,22 @@ submitExcusesButton.addEventListener("click", function () {
       selectedExcuses = data[0].excuse;
       console.log(selectedExcuses);
       let selectedExcuseElement = document.getElementById("addExcuses");
+      selectedExcuseElement.classList.remove("hidden");
       // selectedExcuseElement.innerText = selectedExcuses;
       // selectedExcuseElement.classList.add("addFirstExcuses");
-      const para = document.createElement("p");
+      let para = document.createElement("p");
+      para.style.cssText = 'margin:2px;';
+
+      console.log(selectedExcuseElement.children.length);
+      if(selectedExcuseElement.children.length > 0){
+        para.classList.add("border-top");
+
+      }
+
+
       const node = document.createTextNode(selectedExcuses);
       para.appendChild(node);
-
-      const element = document.getElementById("addExcuses");
+      let element = document.getElementById("addExcuses");
       element.appendChild(para);
-      return selectedExcuses;
     });
 });
